6e6d94cba07cd596e8103d1bb562aa8df1d5d5fc
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-08-02 17:32:16 -0500

    Add header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../Header';
 
 class App extends Component {
   render() {
     return (
       <div>
-
+          <Header
+            title = 'React News' />
       </div>
     );
   }
